69f1c6de4f29f04ebb19050e3390d9f62a4bb8e4
Shiyuan Huang <shiyuancn@live.cn>
2023-01-07 22:28:23 -0500

    Refactor Day 2

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -1,5 +1,5 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::BufRead;
+use crate::days::util;
 
 pub fn main() {
     part1();
@@ -7,62 +7,72 @@ pub fn main() {
 }
 
 fn part1() {
-    let file = File::open("inputs/day2.txt").unwrap();
-    let reader = BufReader::new(file);
-    let x = 'X' as i32;
-    let a = 'A' as i32;
-    let mut total_score = 0;
-
-    for line in reader.lines() {
-        let line = line.unwrap();
-        let vec: Vec<&str> = line.split(" ").collect();
-        let your_offset = vec[1].chars().nth(0).unwrap() as i32 - x;
-        let opponent_offset = vec[0].chars().nth(0).unwrap() as i32 - a;
-        let mut score = 0;
-        let diff = your_offset - opponent_offset;
-        if diff == 0 {
-            score = 3;
-        } else if diff == 1 || diff == -2 {
-            score = 6;
-        }
-        score += your_offset + 1;
-        total_score += score;
-    }
+    let scores: Vec<i32> =
+        parse_guide()
+            .iter()
+            .map(|&(opp_shape, your_shape)| {
+                let diff = your_shape - opp_shape;
+                let outcome_score =
+                    match diff {
+                        0 => 3, // tie
+                        1 | -2 => 6, // win
+                        _ => 0, // loss
+                    };
+                let shape_score = your_shape + 1;
+                outcome_score + shape_score
+            })
+            .collect();
+    let total_score: i32 = scores.iter().sum();
     println!("Total score: {}", total_score);
 }
 
 fn part2() {
-    let file = File::open("inputs/day2.txt").unwrap();
-    let reader = BufReader::new(file);
+    let scores: Vec<i32> =
+        parse_guide()
+            .iter()
+            .map(|&(opp_shape, outcome)| {
+                let outcome_score = outcome * 3;
+                let shape_score =
+                    match outcome {
+                        0 => {
+                            // if you lose, you played the shape that lost to your opponent
+                            if opp_shape >= 1 { opp_shape } else { 3 }
+                        }
+                        1 => {
+                            // if you tie, you played the same shape as your opponent
+                            opp_shape + 1
+                        }
+                        _ => {
+                            // if you win, you played the shape that beat your opponent
+                            if opp_shape < 2 { opp_shape + 2 } else { 1 }
+                        }
+                    };
+                outcome_score + shape_score
+            })
+            .collect();
+    let total_score: i32 = scores.iter().sum();
+    println!("Total score: {}", total_score)
+}
+
+// Returns: a pair of offsets for each line in the strategy guide
+// (0, 0) represents ('A', 'X'), (1, 1) => ('B', 'Y'), (2, 2) => ('C', 'Z')
+fn parse_guide() -> Vec<(i32, i32)> {
+    let reader = util::get_day_reader(2);
     let x = 'X' as i32;
     let a = 'A' as i32;
-    let mut total_score = 0;
-
-    for line in reader.lines() {
-        let line = line.unwrap();
-        let vec: Vec<&str> = line.split(" ").collect();
-        let your_offset = vec[1].chars().nth(0).unwrap() as i32 - x;
-        let opponent_offset = vec[0].chars().nth(0).unwrap() as i32 - a;
-        let mut score = your_offset * 3;
-        if your_offset == 1 {
-            // if you tie, you played the same shape as your opponent
-            score += opponent_offset + 1;
-        } else if your_offset == 0 {
-            // if you lose, you played the shape that lost to your opponent
-            if opponent_offset >= 1 {
-                score += opponent_offset;
-            } else {
-                score += 3;
-            }
-        } else {
-            // if you win, you played the shape that beat your opponent
-            if opponent_offset < 2 {
-                score += opponent_offset + 2;
-            } else {
-                score += 1;
-            }
-        }
-        total_score += score;
-    }
-    println!("Total score: {}", total_score)
+    let lines: Vec<(char, char)> =
+        reader
+            .lines()
+            .filter(|x| !x.as_ref().unwrap().is_empty())
+            .map(|x| {
+                let chars: Vec<char> = x.unwrap().chars().collect();
+                (chars[0], chars[2])
+            })
+            .collect();
+    let offsets: Vec<(i32, i32)> =
+        lines
+            .iter()
+            .map(|(c1, c2)| (*c1 as i32 - a, *c2 as i32 - x))
+            .collect();
+    return offsets;
 }
