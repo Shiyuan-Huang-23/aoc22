9e1983bffe87f658539fea89a31ef501a41cfa44
Shiyuan Huang <shiyuancn@live.cn>
2023-01-08 12:34:08 -0500

    Refactor Day 3

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -1,5 +1,5 @@
 use std::collections::HashSet;
-use std::io::{BufRead};
+use std::io::BufRead;
 use crate::days::util;
 
 pub fn main() {
@@ -9,66 +9,66 @@ pub fn main() {
 
 fn part1() {
     let reader = util::get_day_reader(3);
-    let mut first = HashSet::new();
-    let mut second = HashSet::new();
-    let mut total_priority = 0;
-    for line in reader.lines() {
-        let line = line.unwrap();
-        let vec: Vec<&str> = line.split("").collect();
-        let n = vec.len() / 2;
-        for (i, s) in vec.iter().enumerate() {
-            if i < n {
-                first.insert(s.to_string());
-            } else {
-                second.insert(s.to_string());
-            }
-        }
-        let intersection: HashSet<_> = first.intersection(&second).collect();
-        total_priority += get_priority(intersection);
-        first.clear();
-        second.clear();
-    }
+    let lines: Vec<String> = reader.lines().map(|x| x.unwrap()).collect();
+    let mut seen = HashSet::new();
+    let priorities: Vec<i32> =
+        lines
+            .iter()
+            .map(|s| {
+                let mut res = 0;
+                let n = s.len() / 2;
+                for (i, c) in s.chars().enumerate() {
+                    if i < n {
+                        seen.insert(c);
+                    } else if seen.contains(&c) {
+                        res = get_priority(c);
+                        seen.clear();
+                        break;
+                    }
+                };
+                res
+            })
+            .collect();
+    let total_priority: i32 = priorities.iter().sum();
     println!("Total priority: {}", total_priority);
 }
 
-// 2694 is too high
 fn part2() {
     let reader = util::get_day_reader(3);
-    let mut set: HashSet<String> = HashSet::new();
-    let mut total_priority = 0;
-    for (i, line) in reader.lines().enumerate() {
-        let line = line.unwrap();
-        let vec: Vec<&str> = line.split("").collect();
-        let mut curr_set: HashSet<String> = HashSet::new();
-        for s in vec.iter() {
-            curr_set.insert(s.to_string());
-        }
-        if i % 3 == 0 {
-            set = curr_set;
-        } else {
-            let intersection: HashSet<&String> = set.intersection(&curr_set).collect();
-            if i % 3 == 2 {
-                total_priority += get_priority(intersection);
-                set.clear();
-            } else {
-                set = intersection.iter().map(|x| x.to_string()).collect()
-            }
+    let lines: Vec<String> = reader.lines().map(|x| x.unwrap()).collect();
+    // item types carried by each group
+    let mut groups: Vec<Vec<HashSet<char>>> = Vec::new();
+    let mut curr_group: Vec<HashSet<char>> = Vec::new();
+    for line in lines {
+        curr_group.push(HashSet::from_iter(line.chars()));
+        if curr_group.len() == 3 {
+            groups.push(curr_group.clone());
+            curr_group.clear();
         }
     }
+    let priorities: Vec<i32> =
+        groups
+            .iter()
+            .map(|group| {
+                // find item type held by all three elves
+                let temp: HashSet<char> =
+                    group[0]
+                        .intersection(&group[1])
+                        .map(|&x| x)
+                        .collect();
+                let intersection: HashSet<&char> = temp.intersection(&group[2]).collect();
+                let c = intersection.iter().next().unwrap();
+                get_priority(**c)
+            })
+            .collect();
+    let total_priority: i32 = priorities.iter().sum();
     println!("Total priority: {}", total_priority);
 }
 
-fn get_priority(set: HashSet<&String>) -> i32 {
-    for s in set.iter() {
-        if !s.is_empty() {
-            let c = s.chars().nth(0).unwrap();
-            return if c.is_lowercase() {
-                c as i32 - 96
-            } else {
-                // -38 = - 64 + 26
-                c as i32 - 38
-            }
-        }
-    }
-    return 0;
+fn get_priority(c: char) -> i32 {
+    return if c.is_lowercase() {
+        c as i32 - 'a' as i32 + 1
+    } else {
+        c as i32 - 'A' as i32 + 27
+    };
 }
